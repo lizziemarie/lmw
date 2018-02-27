258549c8b18403a723e9937f9e4ed8639a533ef2
lizzie marie <elizabethmariewillett@gmail.com>
2018-02-27 09:16:12 -0800

    case study copy added

--- wp-content/themes/lmw-theme/single-case_studies.php
+++ wp-content/themes/lmw-theme/single-case_studies.php
@@ -15,56 +15,60 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post();
 				$services = get_field('services');
-				$the_problem = get_field('the_problem');
-				$the_solution = get_field('the_solution');
-				$the_outcome = get_field('the_outcome');
 				$image_1 = get_field('image_1');
 				$image_2 = get_field('image_2');
 				$image_3 = get_field('image_3');
 				$image_4 = get_field('image_4');
 				$image_5 = get_field('image_5');
-				$size = "full"; ?>
+				$size = "full";
+				$label_1 = get_field('label_1');
+				$label_2 = get_field('label_2');
+				$label_3 = get_field('label_3');
+				$label_4 = get_field('label_4');
+				$paragraph_1 = get_field('paragraph_1');
+				$paragraph_2 = get_field('paragraph_2');
+				$paragraph_3 = get_field('paragraph_3');
+				$paragraph_4 = get_field('paragraph_4');?>
 
         <section class="case-study">
 				  <div class="case-study-intro">
 							<h1><?php the_title(); ?></h1>
 							<h3><?php echo $services; ?></h3>
-
 							<?php the_content(); ?>
+					</div> <!-- .case-study-intro -->
 
-							<!-- <p class="read-more-link"><a href="<?php /*echo $link; */?>">Visit Live Site</a></p> -->
-					</div>
-				</section>
 					<div class="case-study-detail">
 						<?php if($image_1) {
 							echo wp_get_attachment_image( $image_1, $size );
 						} ?>
-						<h2>The Problem</h2>
-						<?php echo $the_problem; ?>
 
-						<h2>The Solution</h2>
-						<?php echo $the_solution; ?>
+						<?php echo $label_1; ?>
+						<?php echo $paragraph_1; ?>
+
+						<?php echo $label_2; ?>
+						<?php echo $paragraph_2; ?>
 
 						<?php if($image_2) {
 							echo wp_get_attachment_image( $image_2, $size );
 						} ?>
-
 						<?php if($image_3) {
 							echo wp_get_attachment_image( $image_3, $size );
 						} ?>
-
 						<?php if($image_4) {
 							echo wp_get_attachment_image( $image_4, $size );
 						} ?>
-
 						<?php if($image_5) {
 							echo wp_get_attachment_image( $image_5, $size );
 						} ?>
 
-						<h2>The Outcome</h2>
-						<?php echo $the_outcome; ?>
-					</div>
+						<?php echo $label_3; ?>
+						<?php echo $paragraph_3; ?>
 
+						<?php echo $label_4; ?>
+						<?php echo $paragraph_4; ?>
+					</div> <!--.case-study-detail-->
+
+				</section> <!--.case-study-->
 
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
