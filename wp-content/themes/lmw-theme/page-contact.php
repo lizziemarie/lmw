660c1f1f916e83719736fc99608215e17d4eb04c
lizzie marie <elizabethmariewillett@gmail.com>
2018-02-27 14:36:55 -0800

    update contact page

--- wp-content/themes/lmw-theme/page-contact.php
+++ wp-content/themes/lmw-theme/page-contact.php
@@ -17,12 +17,16 @@ get_header(); ?>
 		<main id="main" class="site-main">
 
       <section class="contact-main">
-        <div>
+        <div class="contact-item-1">
           <h2>Work with me</h2>
+				</div>
+				<div class="contact-item-2">
           <p>Enough about me! What can we work on together? I love working with new people to help solve fun and challenging problems. Let's grab tea or coffee and talk about how we can work together.</p>
         </div>
-        <div>
+        <div class="contact-item-3">
           <h2>Let's chat</h2>
+				</div>
+				<div class="contact-item-4">
           <p>Contact me via email at</p>
           <p><strong>hello@lizziemariew.com</strong></p>
         </div>
